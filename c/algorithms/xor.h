0224a99a38dad4e3ade5c24e361a46cc4d995335
Justin Dobson <jed833@gmail.com>
2014-09-08 23:35:17 -0700

    still working on the Test

--- c/algorithms/xor.h
+++ c/algorithms/xor.h
@@ -5,9 +5,9 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
-int genKey( int key )
+int* genKey( int key )
 {
-    printf("generating key\n" );
+    printf( "generating key\n" );
 
     int randData = open("/dev/random", O_RDONLY );
     int randInt;
@@ -26,34 +26,47 @@ int genKey( int key )
 
     }
     close( randData );
+  
+    int ar_key[key];
+    int i = 0;
+    for( i = 0; i < key; i++ )
+    {
+        ar_key[i] = key;
+    }
 
-    return key * randInt;
+    printf("ar_key: %d\n", ar_key[0] );
+
+    return ar_key;
 }
 
-int* xorStrings( char *message, int key, int length)
+char* xorStrings( char *message, int *key, int length)
 {
-    int ar[length];
+    char ar[length];
     int i = 0;
     for( i = 0; i < length; i++)
     {
-        ar[i] = (int)(message[i] ^ key);
+        ar[i] = message[i] ^ key[i];
+        printf( "ar[%d]: %x\n", i, ar[i] );
+        printf( "key: %d\n", key[i] );
     }
 
     return ar;
 }
 
-char* decryptString( int *ar, int key, int length )
+char* decryptString( char *ar, int *key, int length )
 {
+    printf("%x\n", ar[0]);
     char mess[length];
     int i = 0;
     for( i = 0; i < length; i++ )
     {
-        mess[i] = (char)(ar[i] ^ key);
+        mess[i] = ar[i] ^ key[i];
+        printf( "DE_ar[0]: %x\n", ar[i] );
+        printf( "key: %d\n", key[i] );
     }
 
     return mess;
 }
-
 
 
 #endif
--- c/algorithms/xorTest.c
+++ c/algorithms/xorTest.c
@@ -8,40 +8,41 @@ int main()
     int _ar_i;
     
     scanf( "%d", &_ar_size );
+    printf( "%d\n", _ar_size ); 
 
-    _ar_size += 1;
-    
     char _ar[_ar_size];
-    int _ar_en[_ar_size];
-    char _ar_de[_ar_size];
+    char *_ar_en;
+    int *key;
+    char *_ar_de;
     
     for( _ar_i = 0; _ar_i < _ar_size; _ar_i++)
     {
         scanf( "%s", &_ar[_ar_i] );
+        printf( "%x\n", _ar[_ar_i] );
     }
 
     _ar_i = 0;
 
-    int key = genKey(_ar_size);
+    key = genKey( _ar_size );
 
-    printf("%d\n", key);
+    printf("key: %d\n", key[0]);
 
-    *_ar_en = xorStrings( _ar, key, _ar_size);
+    _ar_en = xorStrings( _ar, key, _ar_size);
 
     for( _ar_i = 0; _ar_i < _ar_size; _ar_i++)
     {
-        printf( "%s", &_ar_en[_ar_i] );
+        printf( "EN: %x", _ar_en[_ar_i] );
     }
 
     printf("\n");
 
     _ar_i = 0;
 
-    *_ar_de = decryptString( _ar_en, key, _ar_size);
+    _ar_de = decryptString( _ar_en, key, _ar_size);
     
     for( _ar_i = 0; _ar_i < _ar_size; _ar_i++ )
     {
-        printf( "%s", &_ar_de[_ar_i] );
+        printf( "DE: %x\n", _ar_de[_ar_i] );
     }
 
     printf("done");
