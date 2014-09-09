a028eafbf6f41dcee916b0893d492ed8b0523ad6
Justin <jed833@gmail.com>
2014-09-08 17:38:40 -0700

    xor cypher

---
+++ c/algorithms/xor.h
@@ -0,0 +1,45 @@
+#ifndef _XOR__H_
+#define _XOR__H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int genKey( int key )
+{
+    printf("generating key\n" );
+
+    int randData = open("/dev/random", O_RDONLY );
+    int randInt;
+    
+    size_t randLength = 0;
+
+    while( randLength < sizeof( randInt ) )
+    {
+        ssize_t result = read( randData , ((char *)&randInt) + randLength, (sizeof( randInt)) - randLength );
+        if( result < 0 )
+        {
+            printf( "error reading /dev/random\n");
+            return 1;
+        }
+        randLength += result;
+
+    }
+    close( randData );
+
+    return key * randInt;
+}
+
+int xorStrings( char *message, int key, int length)
+{
+    int ar[length];
+    int i = 0;
+    for( i = 0; i < length; i++)
+    {
+        ar[i] = message[i] ^ key;
+    }
+
+    return *ar;
+}
+
+
+#endif
