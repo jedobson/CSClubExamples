a9bb3afd1d80182232354d028aacceae1055dc9e
Justin <jed833@gmail.com>
2015-03-22 03:36:37 -0700

    update to c++

--- c++/structs/linkedlist.cpp
+++ c++/structs/linkedlist.cpp
@@ -1,6 +1,7 @@
 /* linked list library in c++ */
 #include<iostream>
 #include<cstdlib>
+#include<stdio.h>
 
 class linkedList
 {
@@ -14,10 +15,10 @@ class linkedList
     public:
         ~linkedList();
         void clear();
-        node* genNode(int);
         int insertNode(node*, int);
         void removeNode(node*, node*);
         void reverseList();
+        void iterateList();
 
     private:
         node* head = NULL;
@@ -37,16 +38,6 @@ void linkedList::clear()
         delete head;
         head = t;
     }
-}
-
-
-linkedList::node* linkedList::genNode( int item )
-{
-    linkedList::node* n = (linkedList::node*)malloc(sizeof *n);
-    n->item = item;
-    n->next = NULL;
-
-    return n;
 }
 
 int linkedList::insertNode( linkedList::node* i, int n )
@@ -90,7 +81,8 @@ void linkedList::removeNode(linkedList::node* t, linkedList::node* x)
         t->next = NULL;
     }
 
-    free(x);
+    delete x;
+
 }
 
 void linkedList::reverseList()
@@ -111,5 +103,44 @@ void linkedList::reverseList()
 
 }
 
+void linkedList::iterateList()
+{
+    linkedList::node* i = head;
+    while( i != NULL )
+    {
+        printf("%d\n", i->item);
+        i = i->next;
+    }
+}
 
-int main(){}
+
+int main()
+{
+    printf("starting LL test\n");
+    int x = 1;
+    int pos = 0;
+
+    linkedList ll;
+    linkedList::node* node1 = new linkedList::node;
+    node1->item = x;
+    x++;
+    if(!ll.insertNode(node1, pos))
+    {
+        return 0;
+    }
+    pos++;
+
+    linkedList::node* node2 = new linkedList::node;
+    node2->item = x;
+    x++;
+    if(!ll.insertNode(node2, pos))
+    {
+        return 0;
+    }
+    pos++;
+
+    ll.iterateList();
+    ll.reverseList();
+    ll.iterateList();
+
+}
